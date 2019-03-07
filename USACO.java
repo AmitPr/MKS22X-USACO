d1fba4457e7839360492c3768b2354a4b825dc31
AmitPr <17amitprasad@gmail.com>
2019-03-07 11:00:24 -0500

    finished silver()

--- USACO.java
+++ USACO.java
@@ -5,7 +5,8 @@ import java.io.File;
 public class USACO {
     public static void main(String[] args) {
         try {
-            //System.out.println(bronze("TestCases/makelake.5.in"));
+            System.out.println(bronze("TestCases/makelake.5.in"));
+            System.out.println(silver("TestCases/ctravel.5.in"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -17,6 +18,7 @@ public class USACO {
         String init = s.nextLine();
         String[] args = init.split(" ");
         if (args.length != 4) {
+            s.close();
             throw new IllegalArgumentException();
         }
         int r, c, e, n = 0;
@@ -68,27 +70,93 @@ public class USACO {
             for (int x = 0; x < c; x++) {
                 if (pasture[y][x] < e) {
                     pasture[y][x] = e - pasture[y][x];
-                    total+=pasture[y][x];
+                    total += pasture[y][x];
                 } else {
                     pasture[y][x] = 0;
                 }
             }
         }
-        return total*72*72;
+        s.close();
+        return total * 72 * 72;
     }
 
     public static String arrToString(int[][] arr) {
+        int spacer = 5;
         String s = "";
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                s += arr[i][j] + " ";
+                int x = arr[i][j];
+                int len = (x + "").length();
+                for (int l = len; l < spacer; l++) {
+                    s += " ";
+                }
+                s += x;
             }
             s += "\n";
         }
         return s;
     }
 
-    public static int silver(String filename) {
-        return 0;
+    private static char[][] pasture = null;
+
+    public static int silver(String filename) throws IOException {
+        File f = new File(filename);
+        Scanner s = new Scanner(f);
+        String init = s.nextLine();
+        String[] args = init.split(" ");
+        if (args.length != 3) {
+            s.close();
+            throw new IllegalArgumentException();
+        }
+        int n, m, t = 0;
+        n = Integer.parseInt(args[0]);
+        m = Integer.parseInt(args[1]);
+        t = Integer.parseInt(args[2]);
+        pasture = new char[n][m];
+        int[][] possible = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String line = s.nextLine();
+            int j = 0;
+            for (int c = 0; c < line.length(); c++) {
+                char val = line.charAt(c);
+                pasture[i][j] = val;
+                j++;
+            }
+        }
+        int r1, c1, r2, c2 = 0;
+        args = s.nextLine().split(" ");
+        if (args.length != 4) {
+            s.close();
+            throw new IllegalArgumentException();
+        }
+        r1 = Integer.parseInt(args[0]) - 1;
+        c1 = Integer.parseInt(args[1]) - 1;
+        r2 = Integer.parseInt(args[2]) - 1;
+        c2 = Integer.parseInt(args[3]) - 1;
+        possible[r1][c1] = 1;
+        int[][] temp = new int[n][m];
+        for (int p = 0; p < t; p++) {
+            for (int row = 0; row < n; row++) {
+                for (int col = 0; col < m; col++) {
+                    if (row < possible.length-1 && possible[row + 1][col] > 0 && pasture[row + 1][col] == '.') {
+                        temp[row][col] += possible[row + 1][col];
+                    }
+                    if (row > 0 && possible[row - 1][col] > 0&& pasture[row - 1][col] == '.') {
+                        temp[row][col] += possible[row - 1][col];
+                    }
+                    if (col < possible[0].length-1 && possible[row][col+1] > 0&& pasture[row][col+1] == '.') {
+                        temp[row][col] += possible[row][col+1];
+                    }
+                    if (col > 0 && possible[row][col-1] > 0&& pasture[row][col-1] == '.') {
+                        temp[row][col] += possible[row][col-1];
+                    }
+                }
+            }
+            possible = temp;
+            temp = new int[n][m];
+        }
+        s.close();
+        int answer = possible[r2][c2];
+        return answer;
     }
 }
